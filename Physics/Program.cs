44cd524d43d0c3ea07cd69305b60f41757309ff2
myamano <nbz.orenge@gmail.com>
2020-09-29 10:44:29 +0900

    add 円と三角形の衝突判定(辺)

--- Physics/Program.cs
+++ Physics/Program.cs
@@ -5,6 +5,11 @@ namespace Physics
 {
     class Program
     {
+        protected static float Dot(Vector2F v1, Vector2F v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y;
+        }
+
         static void Main(string[] args)
         {
             Engine.Initialize("Phys2D", 960, 720);
@@ -79,8 +84,21 @@ namespace Physics
                     || (triangle.Point2 - circle.Position).Length < circle.Radius
                     || (triangle.Point3 - circle.Position).Length < circle.Radius) circle.Color = intersect;
 
+                var AO = circle.Position - triangle.Point1;
+                var BO = circle.Position - triangle.Point3;
+                var CO = circle.Position - triangle.Point2;
+                var AB = triangle.Point3 - triangle.Point1;
+                var BC = triangle.Point2 - triangle.Point3;
+                var CA = triangle.Point1 - triangle.Point2;
+                var dist1 = Math.Abs(Vector2F.Cross(AO, AB)) / AB.Length;
+                var dist2 = Math.Abs(Vector2F.Cross(BO, BC)) / BC.Length;
+                var dist3 = Math.Abs(Vector2F.Cross(CO, CA)) / CA.Length;
+                if (dist1 <= circle.Radius && Dot(AO, AB) * Dot(BO, AB) <= 0) circle.Color = intersect;
+                if (dist2 <= circle.Radius && Dot(BO, BC) * Dot(CO, BC) <= 0) circle.Color = intersect;
+                if (dist3 <= circle.Radius && Dot(CO, CA) * Dot(AO, CA) <= 0) circle.Color = intersect;
+
                 // 移動
-                if(Engine.Keyboard.GetKeyState(Key.Right) == ButtonState.Hold)
+                if (Engine.Keyboard.GetKeyState(Key.Right) == ButtonState.Hold)
                 {
                     circle.Position += new Vector2F(5f, 0);
                 }
