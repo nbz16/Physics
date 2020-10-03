c3ed7407ddfe08667aaff5c1c006ef6558baba88
myamano <nbz.orenge@gmail.com>
2020-10-03 23:26:47 +0900

    fix 変数名の変更

--- Physics/Program.cs
+++ Physics/Program.cs
@@ -21,13 +21,13 @@ namespace Physics
             Color normalColor = new Color(100, 130, 180);
 
             //プレイヤー
-            var circle = new CircleNode();
-            circle.Radius = 70;
-            circle.Position = new Vector2F(100, 100);
-            circle.Color = normalColor;
-            circle.VertNum = 100;
+            var player = new CircleNode();
+            player.Radius = 70;
+            player.Position = new Vector2F(100, 100);
+            player.Color = normalColor;
+            player.VertNum = 100;
             //circle.ZOrder = 1;
-            Engine.AddNode(circle);
+            Engine.AddNode(player);
 
             //円の相手
             var other = new CircleNode();
@@ -72,61 +72,61 @@ namespace Physics
                 Engine.Update();
 
                 //speed += new Vector2F(0f, gravity/100);
-                circle.Position += new Vector2F(0f,1f);
-                if (circle.Position.Y > 400f) circle.Position = new Vector2F(circle.Position.X, 400f);
+                player.Position += new Vector2F(0f,1f);
+                if (player.Position.Y > 400f) player.Position = new Vector2F(player.Position.X, 400f);
 
                 //円の衝突判定
-                float len = (circle.Position - other.Position).Length;
-                float dist = circle.Radius + other.Radius;
+                float len = (player.Position - other.Position).Length;
+                float dist = player.Radius + other.Radius;
                 if(len <= dist)
                 {
-                    circle.Color = intersect;
+                    player.Color = intersect;
 
                     //簡単な衝突応答
-                    var dir = (other.Position - circle.Position);
+                    var dir = (other.Position - player.Position);
                     //中心間を結んだベクトルの方向に、めり込んだ分だけ移動
-                    other.Position += dir.Normal * (circle.Radius + other.Radius - dir.Length);
+                    other.Position += dir.Normal * (player.Radius + other.Radius - dir.Length);
                 }
                 else
                 {
-                    circle.Color = normalColor;
+                    player.Color = normalColor;
                 }
 
                 //円と三角形の衝突判定
-                if ((triangle.Point1 - circle.Position).Length < circle.Radius
-                    || (triangle.Point2 - circle.Position).Length < circle.Radius
-                    || (triangle.Point3 - circle.Position).Length < circle.Radius) circle.Color = intersect;
+                if ((triangle.Point1 - player.Position).Length < player.Radius
+                    || (triangle.Point2 - player.Position).Length < player.Radius
+                    || (triangle.Point3 - player.Position).Length < player.Radius) player.Color = intersect;
 
-                var AO = circle.Position - triangle.Point1;
-                var BO = circle.Position - triangle.Point3;
-                var CO = circle.Position - triangle.Point2;
+                var AO = player.Position - triangle.Point1;
+                var BO = player.Position - triangle.Point3;
+                var CO = player.Position - triangle.Point2;
                 var AB = triangle.Point3 - triangle.Point1;
                 var BC = triangle.Point2 - triangle.Point3;
                 var CA = triangle.Point1 - triangle.Point2;
                 var dist1 = Math.Abs(Vector2F.Cross(AO, AB)) / AB.Length;
                 var dist2 = Math.Abs(Vector2F.Cross(BO, BC)) / BC.Length;
                 var dist3 = Math.Abs(Vector2F.Cross(CO, CA)) / CA.Length;
-                if (dist1 <= circle.Radius && Dot(AO, AB) * Dot(BO, AB) <= 0) circle.Color = intersect;
-                if (dist2 <= circle.Radius && Dot(BO, BC) * Dot(CO, BC) <= 0) circle.Color = intersect;
-                if (dist3 <= circle.Radius && Dot(CO, CA) * Dot(AO, CA) <= 0) circle.Color = intersect;
-                if (Vector2F.Cross(AO, AB) <= 0 && Vector2F.Cross(BO, BC) <= 0 && Vector2F.Cross(CO, CA) <= 0) circle.Color = intersect;
+                if (dist1 <= player.Radius && Dot(AO, AB) * Dot(BO, AB) <= 0) player.Color = intersect;
+                if (dist2 <= player.Radius && Dot(BO, BC) * Dot(CO, BC) <= 0) player.Color = intersect;
+                if (dist3 <= player.Radius && Dot(CO, CA) * Dot(AO, CA) <= 0) player.Color = intersect;
+                if (Vector2F.Cross(AO, AB) <= 0 && Vector2F.Cross(BO, BC) <= 0 && Vector2F.Cross(CO, CA) <= 0) player.Color = intersect;
 
                 // 移動
                 if (Engine.Keyboard.GetKeyState(Key.Right) == ButtonState.Hold)
                 {
-                    circle.Position += new Vector2F(5f, 0);
+                    player.Position += new Vector2F(5f, 0);
                 }
                 if (Engine.Keyboard.GetKeyState(Key.Left) == ButtonState.Hold)
                 {
-                    circle.Position += new Vector2F(-5f, 0);
+                    player.Position += new Vector2F(-5f, 0);
                 }
                 if (Engine.Keyboard.GetKeyState(Key.Up) == ButtonState.Hold)
                 {
-                    circle.Position += new Vector2F(0, -5f);
+                    player.Position += new Vector2F(0, -5f);
                 }
                 if (Engine.Keyboard.GetKeyState(Key.Down) == ButtonState.Hold)
                 {
-                    circle.Position += new Vector2F(0, 5f);
+                    player.Position += new Vector2F(0, 5f);
                 }
 
                 //終了
